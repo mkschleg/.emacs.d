5cedaf98fa7491a4fa0d3dbf3f320e18a83f4236
Matthew Schlegel <mkschleg@gmail.com>
2022-10-25 15:07:57 -0600

    Added init-org-babel

---
+++ lisp/init-org-babel.el
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+;; (use-package 
+;;   :ensure t
+;;   :custom
+
+;; (org-babel-do-load-languages
+;;  'org-babel-load-languages
+;;  '((emacs-lisp . t)
+;;    (python . t)))
+;;   )
+
+;; (use-package ob-julia
+;;   :ensure t)
+
+
+(provide 'init-org-babel)
