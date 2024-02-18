05399bc2582b60f55b2cd7573872b645052560b2
Matthew Schlegel <matt@ChunkyBoi.lan>
2024-02-18 09:10:41 -0700

    Small updates

--- lisp/init-project.el
+++ lisp/init-project.el
@@ -1,17 +1,23 @@
 
 
 
-(use-package projectile
-  :ensure t
-  :custom
-  (projectile-completion-system 'ivy)
-  (projectile-mode +1))
+;; (use-package projectile
+;;   :ensure t
+;;   :custom
+;;   (projectile-completion-system 'ivy)
+;;   (projectile-mode +1)
+;;   :config
+  
+;;   (advice-add 'projectile-project-root :before-while
+;;               (lambda (&optional dir)
+;;                 (not (file-remote-p (or dir default-directory)))))
+;;   )
 
-(use-package counsel-projectile
-  :ensure t)
+;; (use-package counsel-projectile
+;;   :ensure t)
 
-(root-leader
-  "r" '(:keymap projectile-command-map :which-key "projectile"))
+;; (root-leader
+;;   "r" '(:keymap projectile-command-map :which-key "projectile"))
 
 (provide 'init-project)
 
