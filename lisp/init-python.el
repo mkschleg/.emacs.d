826da28b277ddb4c0843a248673507dd6c1b14f9
Matthew Schlegel <matt@Lapboi.local>
2024-03-31 12:06:41 -0600

    Treesitter mode

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,26 +1,30 @@
 
 
-(use-package elpy
-  :straight t
-  :init
-  (elpy-enable))
+;; (use-package elpy
+;;   :straight t
+;;   :init
+;;   (elpy-enable))
+
+;; (use-package 
+
+
 
 ;; (use-package elpy
 ;;   :straight t
 ;;   :init
 ;;   (elpy-enable 1))
 
-(use-package anaconda-mode
-  :straight t
-  :hook ((python-mode . anaconda-mode)
-	 (python-mode . anaconda-eldoc-mode)))
+;; (use-package anaconda-mode
+;;   :straight t
+;;   :hook ((python-mode . anaconda-mode)
+;; 	 (python-mode . anaconda-eldoc-mode)))
 
 (require 'rx)
 
-(use-package company-anaconda
-  :straight t
-  :init
-  (add-to-list 'company-backends 'company-anaconda))
+;; (use-package company-anaconda
+;;   :straight t
+;;   :init
+;;   (add-to-list 'company-backends 'company-anaconda))
 
 ;; (use-package conda
 ;;   :straight t
@@ -34,12 +38,12 @@
   (setenv "WORKON_HOME" "~/.pyenv/shims/")
   (pyvenv-mode 1))
 
-(use-package pipenv
-  :straight t
-  :hook (python-mode . pipenv-mode)
-  :init
-  (setq
-   pipenv-projectile-after-switch-function
-   #'pipenv-projectile-after-switch-extended))
+;; (use-package pipenv
+;;   :straight t
+;;   :hook (python-mode . pipenv-mode)
+;;   :init
+;;   (setq
+;;    pipenv-projectile-after-switch-function
+;;    #'pipenv-projectile-after-switch-extended))
 
 (provide 'init-python)
